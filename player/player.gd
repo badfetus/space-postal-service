263cbcbdd3bfb3bcc89f9ac6acfa953fac02887b
unknown <berkezorlu@gmail.com>
2023-04-30 21:29:25 +0200

    improved cargo pickup

--- player/player.gd
+++ player/player.gd
@@ -5,7 +5,6 @@ var expected_velocity
 
 var ropeLength = 15
 var PIECE = preload("res://ropelink.tscn")
-var lastrope
 
 func _ready():
 	contact_monitor = true
@@ -13,10 +12,6 @@ func _ready():
 	angular_damp = 5 #stop rotation in 0.2s 
 	connect("body_entered", on_collision)   
 	
-	var parent = self
-	for i in ropeLength:
-		parent = addPiece(parent, i)
-	lastrope = parent
 
 func _physics_process(delta):
 	if Input.is_action_pressed("Boost"):
@@ -27,24 +22,27 @@ func _physics_process(delta):
 var attached = false
 func on_collision(body):
 	if body.is_in_group("cargo"):
-		if(!attached):
-			print("handling cargo")
-			attached = true
-			handleCargo(body)
-	pass
+		handleCargo(body)
 
 func handleCollisions():
 	var velocity_change = saved_velocity - linear_velocity
 	if(velocity_change.length() > 1500):
-		print("collision: ", velocity_change.length())
+		print("dead, collision speed: ", velocity_change.length())
 	saved_velocity = linear_velocity
 
-func handleCargo(body):
-	var joint = lastrope.get_node("ropeconnect/joint")
-	joint.add_child(body)
-	joint.node_a = lastrope.get_path()
-	joint.node_b = body.get_path()
-	body.position = joint.position
+func handleCargo(cargo: RigidBody2D):
+	if(!attached && linear_velocity.length() < 1000):
+		attached = true
+		var parent = self
+		for i in ropeLength:
+			parent = addPiece(parent, i)
+
+		var joint = parent.get_node("ropeconnect/joint")
+		cargo.global_position = joint.global_position
+		joint.add_child(cargo)
+		joint.node_a = parent.get_path()
+		joint.node_b = cargo.get_path()
+
 
 func addPiece(parent, i):
 	print(i)
